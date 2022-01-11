409258548ab0d1e5c6d12318967aa3bd7382c0ec
Mirlan Zhaparov <mirlan.zhaparov.on@gmail.com>
2022-01-11 22:54:41 +0300

    add task5

---
+++ js/task5.js
@@ -0,0 +1,23 @@
+let a = 100;
+let b = 2;
+console.log(a + b);
+
+let c = 100;
+let d = 2;
+console.log(a - b);
+
+let e = 100;
+let f = 2;
+console.log(a * b);
+
+let g = 100;
+let h = 2;
+console.log(a / b);
+
+let j = 100;
+let k = 2;
+console.log(a % b);
+
+let l = 100;
+let m = 2;
+console.log(a ** b);
